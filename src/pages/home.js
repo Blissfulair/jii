a222081730c4407e8c9e5f823192c119fdd61217
mrbhi <taofeeqworld@hotmail.com>
2021-07-02 09:48:26 -0700

    Top and Menu completed

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,11 @@
 import React, {Component} from "react"
 import Header from "../component/header"
+import Top from "../component/top"
 class Home extends Component{
     render(){
         return(
             <>
+            <Top />
             <Header />
             <h1>this is the home page</h1>
             </>
