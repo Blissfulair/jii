54c501c763c5cd7b2f7a9bf33ec1b46d21c118e8
mrbhi <taofeeqworld@hotmail.com>
2021-07-02 08:44:51 -0700

    JII

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,10 @@
 import React, {Component} from "react"
+import Header from "../component/header"
 class Home extends Component{
     render(){
         return(
             <>
+            <Header />
             <h1>this is the home page</h1>
             </>
         )
