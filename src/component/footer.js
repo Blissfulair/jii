6a14e72f3cd404dd69a006618e5b6bed4b9e097e
Lisa <evbuomwanlisa@gmail.com>
2021-07-05 11:49:48 +0100

    jii

---
+++ src/component/footer.js
@@ -0,0 +1,47 @@
+import React, {Component} from "react"
+import "../css/footer.css"
+import logo from "../images/logo.png"
+class Footer extends Component{
+    render(){
+        return(
+            <>
+            <footer>
+                <div className="footer">
+                <img src ={logo} className="logo" alt ="logo"/>
+                <h3 className="jude">Hon. Jude Ise-Idehen’s Forum</h3>
+                <h4 className="quick">Quick Links</h4>
+                            <nav>
+                                <ul>
+                                    <li>
+                                        <a href="/"className="home" >Home</a>
+                                    </li>
+                                    <li >
+                                        <a href="/" className="aboutus">About</a>
+                                    </li>
+                                    <li>
+                                        <a href="/"className="project" >Project</a>
+                                    </li>
+                                    <li>
+                                        <a href="/"className="contact" >Contact us</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                            <h4 className="newsletter">Subscribe to our newsletter</h4>
+                           < div className="formnewsletter">
+                            <form >
+                            <input type="text" className="Emailaddress" placeholder="Email Address"/>
+                            <button className="subscribe">Subscribe</button>
+                            </form>
+                            </div>
+                </div>
+            <div className="greenfooter">
+                <span>Copyright &copy; 2021 </span>
+            </div>
+            </footer>
+            </>
+        )
+    }
+}
+
+
+export default Footer
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,15 @@
 import React, {Component} from "react"
+import Contact from "../component/contact"
+import Footer from "../component/footer"
 class Home extends Component{
     render(){
         return(
             <>
+            <Contact />
+            <Footer />
             <h1>this is the home page</h1>
             </>
+           
         )
     }
 }
