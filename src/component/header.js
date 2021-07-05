fc4ccb7ae3656ecb1d89889d0484b5018c304c8f
mrbhi <taofeeqworld@hotmail.com>
2021-07-05 06:46:26 -0700

    Top bar, Header, Main, About and Project

--- src/component/header.js
+++ src/component/header.js
@@ -23,7 +23,7 @@ class Header extends Component{
     render (){
         return(
             <>
-                <header className ="header">
+                <header>
                     <div className="logo">
                         <img src={logo } alt="LOGO" />
                         <span>
--- src/pages/home.js
+++ src/pages/home.js
@@ -4,7 +4,6 @@ import Header from "../component/header"
 import Main from "../component/main"
 import About from "../component/about"
 import Project from "../component/project"
-// import Contact from "../component/contact"
 class Home extends Component{
     render(){
         return(
@@ -14,8 +13,6 @@ class Home extends Component{
             <Main />
             <About />
             <Project />
-            
-            
             </>
         )
     }
